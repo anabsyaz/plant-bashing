1fbf498c29ebd24a65d79407c78b9dad2c7db69f
Azaan <anabsyaz@gmail.com>
2025-04-30 15:28:32 -0500

    almost done dialog

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -3,5 +3,14 @@ echo "Welcome to THE GAME"
 echo "What is your name?"
 read player_name
 echo "Nice to meet you, $player_name"
-sleep 4
-exit 
+echo would you like to plant a seed? yes/no
+read answer 
+if [[ "$answer" == "yes" ]];
+	then echo "You dug a hole and planted a small seed the size of a olive"
+if [["$answer"=="no"]];
+	then echo thank you for playing;
+	sleep 4;
+	exit;
+echo "In your computers world time moves
+ much faster then real life for example 1 second is 1 minuite in the game
+ and 1 minuite in real life is 1 hour in reality"
